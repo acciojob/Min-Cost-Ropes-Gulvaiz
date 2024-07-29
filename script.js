89cd6ea49bd97df0e4c0235f0740b9266fdcae83
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-07-30 01:19:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-   return arr.sort((a,b) => a - b)
+ let cost = 0;
+while (arr.length > 1) {
+    let temp = arr[0] + arr[1];
+    cost += temp;
+    arr.splice(0, 2);
+    arr.push(temp);
+    arr.sort((a, b) => a - b);
+}
 	
 }
 
